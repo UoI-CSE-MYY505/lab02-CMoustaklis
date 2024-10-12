0e8a2ba15b03f402dc99523b12d84267fcb21ac0
Christos Moustaklis <149680725+CMoustaklis@users.noreply.github.com>
2024-10-12 14:29:47 +0300

    First Commit of Solution
    s1 incremented by 4 ver.

--- lab02.s
+++ lab02.s
@@ -10,8 +10,21 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+
+    add s0, zero, zero
+    add s1, zero, a0
+    beq a1, zero, done
+do_while_loop:
+    lw t0, 0(s1)
+    bne t0, a2, skip
+    add s0, zero, s1
+skip:
+    addi s1, s1, 4
+    add t1, zero, a1
+    slli t1, t1, 2
+    add t1, t1, a0
+    bne s1, t1, do_while_loop
+    
 #-----------------------------
 done:
     addi a7, zero, 10 
